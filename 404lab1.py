f009690e2dbda2f3a58a3a3da61530ca1dd44658
jasonexus <jasonexus7458@gmail.com>
2022-01-10 20:00:40 -0700

    Added the downloading information

--- 404lab1.py
+++ 404lab1.py
@@ -2,4 +2,7 @@ import requests
 print(requests.__version__)
 url = "https://www.google.com/"
 res = requests.get(url)
-print(res.status_code)
+my_url = "https://raw.githubusercontent.com/jasonexus/CMPUT404Lab1/master/404lab1.py"
+r = requests.get(my_url)
+source_code = r.text
+print(source_code)
